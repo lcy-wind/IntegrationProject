f204563ed174e2eaa119f869c32a89ebc5812926
lcy-wind <lcy083939@163.com>
2020-07-30 17:47:11 +0800

    测试连接失败

--- src/test/java/com/teamwork/integrationproject/IntegrationProjectApplicationTests.java
+++ src/test/java/com/teamwork/integrationproject/IntegrationProjectApplicationTests.java
@@ -1,15 +1,29 @@
 package com.teamwork.integrationproject;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @SpringBootTest
 class IntegrationProjectApplicationTests
 {
 
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void testDataSource() throws SQLException
+    {
+        System.out.println(dataSource.getConnection());
+    }
+
     @Test
     void contextLoads()
     {
+
     }
 
 }
