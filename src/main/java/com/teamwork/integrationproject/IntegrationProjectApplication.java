61d699f782fabdae7464f13c4fd1e04dcf69d6ed
lcy-wind <lcy083939@163.com>
2020-07-30 13:43:05 +0800

    首次提交

---
+++ src/main/java/com/teamwork/integrationproject/IntegrationProjectApplication.java
@@ -0,0 +1,15 @@
+package com.teamwork.integrationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationProjectApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(IntegrationProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamwork/integrationproject/IntegrationProjectApplicationTests.java
@@ -0,0 +1,15 @@
+package com.teamwork.integrationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationProjectApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
